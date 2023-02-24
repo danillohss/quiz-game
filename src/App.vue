3463e6d8fdf2e98e37e5ceb5c5fc91912d4349fd
Danillo Soares <Danillohss@gmail.com>
2023-02-24 00:13:34 -0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,68 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-</template>
+  <div class="app">
+    <div v-if="this.question" class="question">
+      <h1 v-html="this.question"></h1>
+      <template v-for="(answer, index) in this.answers" :key="index">
+        <input v-model="this.chosen_answer" type="radio" name="options" :value="answer" :disabled="this.answerSubmitted">
+        <label v-html="answer"></label><br />
+      </template>
+      <button v-if="!this.answerSubmitted" class="send" @click="submitAnswer">Send</button><br />
 
+      <section class="result" v-if="this.result">
+        <h4 v-if="this.chosen_answer != this.correctAnswer"> Incorrect answer &#10060;</h4>
+        <h4 v-else> Congratulations correct answer &#9989;</h4>
+        <button class="send">Next Question</button>
+      </section>
+
+    </div>
+  </div>
+</template>
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  data() {
+    return {
+      question: null,
+      incorrectAnswers: [],
+      correctAnswer: null,
+      chosen_answer: null,
+      answerSubmitted: false,
+      result: false,
+    }
+  },
+  methods: {
+    submitAnswer() {
+      this.result = true;
+      if (!this.chosen_answer) {
+        alert('Choose one option!')
+      } else {
+        this.answerSubmitted = true;
+        if (this.chosen_answer == this.correctAnswer) {
+          console.log('Correct answer');
+        } else {
+          console.log('Incorrect answer')
+        }
+      }
+    }
+  },
+  computed: {
+    answers() {
+      var answers = JSON.parse(JSON.stringify(this.incorrectAnswers));
+      answers.splice(Math.round(Math.random() * answers.length), 0, this.correctAnswer);
+      return answers;
+    },
+  },
+  async created() {
+    await this.axios.get('https://opentdb.com/api.php?amount=1&category=18')
+      .then((response) => {
+        this.question = response.data.results[0].question;
+        this.incorrectAnswers = response.data.results[0].incorrect_answers;
+        this.correctAnswer = response.data.results[0].correct_answer;
+
+      })
+  },
+
 }
 </script>
 
@@ -21,6 +73,40 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin: 60px auto;
+  max-width: 960px;
+
+}
+
+h1 {
+  margin-top: 40px;
+}
+
+input[type='radio'] {
+  margin: 12px 4px;
+}
+
+button.send {
+  margin-top: 12px;
+  height: 40px;
+  min-width: 120px;
+  padding: 0 16px;
+  color: #fff;
+  background-color: #1867c0;
+  border: 1px solid #1867c0;
+  cursor: pointer;
+  border-radius: 5px;
+}
+
+section.score {
+  border-bottom: 1px solid black;
+  padding: 24px;
+  font-size: 18px;
+
+  span {
+    padding: 8px;
+    font-weight: bold;
+    border: 1px solid black;
+  }
 }
 </style>
